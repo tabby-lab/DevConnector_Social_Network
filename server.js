7f2e8bc595c8361c03dfd3fb41c566d0125febe5
Tabby Garcia <tabby.garcia1@gmail.com>
2020-08-20 14:32:27 -0500

    created config folder with db info to connect to database

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express();
 
+//connect db
+connectDB();
 app.get('/', (req, res)=> res.send('API running'))
 
 const PORT = process.env.PORT || 5000;
