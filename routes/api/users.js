d21042216bf51b5c23f15d65ac234c2c2264c737
Tabby Garcia <tabby.garcia1@gmail.com>
2020-08-24 17:46:46 -0500

    fixed bcrypt err and got avatar to display picture.checking to see if user is registered

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,12 @@
-const express = require ('express');
+const express = require('express');
 const router = express.Router();
 //to validate if the data is correct from Users.js
-const {check, validationResult} = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+const config = require('config')
+
+const User = require('../../models/User');
 
 //@route        POST api/users
 //@description  Register user
@@ -10,25 +15,75 @@ const {check, validationResult} = require('express-validator/check');
 //second parameter forf validator
 //check in documentation express validator
 //VALIDATION
-router.post('/',[
-    check('name', 'Name is required')
-    .not()
-    .isEmpty(),
-    check('email','Please include a valid email').isEmail(),
-    check(
-        'password',
-         'Please enter a password with 6 or more characters')
-         .isLength({ min:6 })
+router.post('/',
+    [
+        check('name', 'Name is required')
+            .not()
+            .isEmpty(),
+        check('email', 'Please include a valid email').isEmail(),
+        check(
+            'password',
+            'Please enter a password with 6 or more characters')
+            .isLength({ min: 6 })
 
-],(req, res) => {
-    // console.log(req.body);
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array() });
+    ],
+    async (req, res) => {
+        // console.log(req.body);
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { name, email, password } = req.body;
+
+
+        try {
+            //see if user exists
+            let user = await User.findOne({ email });
+
+            if (user) {
+                res.status(400).json({ errors: [{ msg: ' User already exists ' }] });
+            }
+
+
+            //get users gravatar
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            });
+
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            });
+
+            //encrypt password using bcrypt
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+
+            await user.save();
+
+            //return jsonwebtoken
+
+            res.send('User register');
+        } catch (err) {
+            console.error(error.message);
+            res.status(500).send('Server error');
+        }
+
+
     }
-    res.send('User route');
-});
+);
 //then test on postman
 //keep endpoints RESTFUL
 
 module.exports = router;
+
+
+
+
