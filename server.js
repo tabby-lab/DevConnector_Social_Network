51ea1f24b136efa27d708e8a4497fd26b4eb1a37
Tabby Garcia <tabby.garcia1@gmail.com>
2020-08-24 10:39:01 -0500

    basic setup for routes and connected to server. tests on postman

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const app = express();
 
 //connect db
 connectDB();
-app.get('/', (req, res)=> res.send('API running'))
+app.get('/', (req, res)=> res.send('API running'));
+
+//access the routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
