d417aa6330f60edb0aeace5c8638d29d0af16fe9
Tabby Garcia <tabby.garcia1@gmail.com>
2020-08-26 14:41:24 -0500

    created a route to delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -153,6 +153,29 @@ router.get('/user/:user_id', async (req,res) => {
 });
 
 
+//@route        DELETE api/profile
+//@description  DELETE profile,user,and post
+//@access       Private
+
+
+router.delete('/', auth, async (req,res) => {
+    try {
+    //@todo-remove users posts
+
+
+    //remove profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+    //remove user
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({msg: 'User deleted'});
+    }catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
 
 
 
