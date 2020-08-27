f2f01d5a2c97fddc1fc14c1955da4a890853580d
Tabby Garcia <tabby.garcia1@gmail.com>
2020-08-27 17:09:22 -0500

    finally got token to render in header, installed axios, updated gitignore

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require('express-validator');
 //for github
 const request = require('request');
 const config = require('config');
+const axios = require('axios');
 
 
 
@@ -365,28 +366,23 @@ try {
 //@description  GET user repos from Github
 //@access       Public
 
-router.get('/github/:username', (req,res) => {
+router.get('/github/:username', async (req, res) => {
     try {
-       const options = {
-           uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
-           method:'GET',
-           headers: {'user-agent': 'node.js'}
-       } ;
-
-       request(options, (error, response, body) => {
-           if(error) console.error(error);
-
-        if(response.statusCode !== 200) {
-            res.status(404).json({ msg: 'No Github profile found'});
-        }
-
-        res.json(JSON.parse(body));
-       });
+      const uri = encodeURI(
+        `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+      );
+      const headers = {
+        'user-agent': 'node.js',
+        Authorization: `token ${config.get('githubToken')}`
+      };
+  
+      const gitHubResponse = await axios.get(uri, { headers });
+      return res.json(gitHubResponse.data);
     } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
+      console.error(err.message);
+      return res.status(404).json({ msg: 'No Github profile found' });
     }
-})
+  });
 
 
 
