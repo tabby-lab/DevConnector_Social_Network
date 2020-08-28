18413b59e00c6e2c83e386c5d5eb5c3451461187
Tabby Garcia <tabby.garcia1@gmail.com>
2020-08-28 14:25:49 -0500

    created delete comment function and tested it on postman. Finally finished with backend.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -219,6 +219,43 @@ router.post('/comment/:id',
     
 });
 
+//@route        DELETE api/posts/comment/:id/:comment_id
+//@description  delete a comment
+//@access       Private
+
+router.delete('/comment/:id/:comment_id', auth, async(req,res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        //Pull out comment
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+        //make sure comment exists
+        if(!comment) {
+            return res.status(404).json({ msg: 'Comment does not exist.'});
+         }
+
+         //check user
+         if(comment.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized.'});
+         }
+
+           //get the remove index
+   const removeIndex = post.likes.map(comment => comment.user.toString()).indexOf(req.user.id);
+
+   post.comments.splice(removeIndex, 1);
+
+        await post.save();
+        res.json(post.comments);
+
+
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error')
+    }
+});
+
 
 
 module.exports = router;
