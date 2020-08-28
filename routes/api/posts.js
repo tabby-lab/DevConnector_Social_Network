454de3862701bbd7cf4ad9ae525fbfa079afbac5
Tabby Garcia <tabby.garcia1@gmail.com>
2020-08-28 09:51:55 -0500

    delete posts completye

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -86,7 +86,40 @@ router.get('/:id', auth, async (req,res) => {
         console.error(err.message);
         res.status(500).send('Server Error');
     }
-})
+});
+
+
+
+//@route        DELETE api/posts/:id
+//@description  delete a post
+//@access       Private
+
+router.delete('/:id', auth, async (req,res) => {
+
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(!post) {
+            return res.status(404).json({ msg: 'Post not found '})
+        }
+
+        if(post.user.toString() !== req.user.id) {
+        return res.status(401).json({ msg: 'User not authorized'}) ;
+       }
+
+       await post.remove();
+
+       res.json({ msg: 'Post removed'})
+
+        
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found '})
+        }
+        res.status(500).send('Server Error');
+    }
+});
 
 
 
