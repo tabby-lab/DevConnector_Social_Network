9ea846ed8444d6de60e2a100f8247830ad19a4db
Tabby Garcia <tabby.garcia1@gmail.com>
2020-08-28 08:41:40 -0500

    added body to post route and checked in postman. had to get rid of /check in express validator for it to run. everything works in postman

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,7 +1,11 @@
 const express = require ('express');
 const router = express.Router();
-const { check, validationResult }  = require('express-validator/check');
+const { check, validationResult }  = require('express-validator');
 const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
 
 //@route        GET api/posts
@@ -16,8 +20,33 @@ router.post('/',
        .isEmpty()
     ] 
  ],
-  (req, res) => {
+  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
 
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+
+    const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+    })
+
+    const post = await newPost.save();
+
+    res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error')
+    }
+
+
+    
 });
 
 
