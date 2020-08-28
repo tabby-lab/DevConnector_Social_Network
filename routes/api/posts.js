3ee002453a77b8b92baa5e89baa8cb34dcd5e0a8
Tabby Garcia <tabby.garcia1@gmail.com>
2020-08-28 10:17:23 -0500

    fixed bug on response

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -135,7 +135,7 @@ router.put('/like/:id', auth, async (req, res) => {
 
     //check if the post has already been liked by this user
     if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
-        return res.json(400).json({ msg: "Post already liked"});
+        return res.status(400).json({ msg: "Post already liked"});
     }
     post.likes.unshift({ user: req.user.id });
         await post.save();
