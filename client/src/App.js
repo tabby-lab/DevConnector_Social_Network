f603eb0c14d0a1d6d8b1988be5e106e357557a79
Tabby Garcia <tabby.garcia1@gmail.com>
2020-09-04 13:18:20 -0500

    installed dependencies i needed but now getting an error on port 5000 already in use

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 
 const App = () => (
+  <Router>
   <Fragment>
     <Navbar />
-    <Landing />
+    <Route exact path='/' component={Landing} />
 
     
   </Fragment>
+  </Router>
 );
  
 
