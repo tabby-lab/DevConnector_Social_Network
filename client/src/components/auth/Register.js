7072efe84ac03a3c9e71ca2eba2ab67cc0627bb4
Tabby Garcia <tabby.garcia1@gmail.com>
2020-09-09 17:32:27 -0500

    created onchange and onsubmit for the form and put onchange for all the values

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -9,17 +9,38 @@ const Register = () => {
     });
 
     const { name, email, password, password2 } = formData;
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const onSubmit = e => {
+        e.preventDefault();
+        if(password !== password2) {
+            console.log('Passwords do not match')
+        } else {
+            console.log(formData)
+        }
+    }
+
     return (
         <Fragment>
                
       <h1 className="large text-primary">Sign Up</h1>
       <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
-      <form className="form" action="create-profile.html">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
-          <input type="text" placeholder="Name" name="name" required />
+          <input 
+          type="text" 
+          placeholder="Name" 
+          name="name" 
+          value={name} 
+          onChange={e => onChange(e)}
+          required />
         </div>
         <div className="form-group">
-          <input type="email" placeholder="Email Address" name="email" />
+          <input 
+          type="email"
+          placeholder="Email Address" 
+          name="email" 
+          value={email} 
+          onChange={e => onChange(e)}/>
           <small className="form-text"
             >This site uses Gravatar so if you want a profile image, use a
             Gravatar email</small
@@ -30,6 +51,8 @@ const Register = () => {
             type="password"
             placeholder="Password"
             name="password"
+            value={password} 
+          onChange={e => onChange(e)}
             minLength="6"
           />
         </div>
@@ -38,6 +61,8 @@ const Register = () => {
             type="password"
             placeholder="Confirm Password"
             name="password2"
+            value={password2} 
+          onChange={e => onChange(e)}
             minLength="6"
           />
         </div>
