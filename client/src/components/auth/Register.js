f455ebe734430b7cbe2b38309af3317bd9e6659a
Tabby Garcia <tabby.garcia1@gmail.com>
2020-09-09 21:03:17 -0500

    did the same thing i did for register but for login and edited out things. trying to figure out why my database isnt loading new users

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
-import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -10,32 +11,18 @@ const Register = () => {
     });
 
     const { name, email, password, password2 } = formData;
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const onChange = e => 
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
     const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2) {
             console.log('Passwords do not match')
         } else {
-            const newUser = {
-                name,
-                email,
-                password
-            }
-            try {
-                const config = {
-                    headers: {
-                        'Content-Type':'application/json'
-                    }
-                }
-
-                const body = JSON.stringify(newUser);
-
-                const res = await axios.post('/api/users', body, config);
-                console.log(res.data);
-            } catch (err) {
-                    console.error(err.response.data);
-            }
+            console.log(formData);
         }
+                
+        
     };
 
     return (
@@ -88,11 +75,12 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
   
         </Fragment>
-    )
-}
+    );
+};
 
-export default Register
+    export default Register;
+    
