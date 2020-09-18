5245638c4408d99c415af853b8a205364516163e
Tabby Garcia <tabby.garcia1@gmail.com>
2020-09-18 15:28:25 -0500

    register success complete in action

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -9,3 +9,18 @@ const initialState = {
     loading: true,
     user: null
 }
+
+export default function(state = initialState, action) {
+    const { type, payload } = action;
+
+switch(type) {
+case REGISTER_SUCCESS:
+    localStorage.setItem('token', payload.token);
+    return {
+        ...state, 
+        ...payload,
+        isAuthenticated: true,
+        loading:false
+    }
+}
+}
