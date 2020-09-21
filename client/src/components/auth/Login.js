ba1b9d204907d1c693e0b51a5fe54ef937f71086
Tabby Garcia <tabby.garcia1@gmail.com>
2020-09-21 16:16:39 -0500

    added login success and login fail to auth actions,same as register just change name. added login success and login fail to types.made a func. to login in login.js and brought in connect, proptypes and login.finally added them to my reducder

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,12 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
 
-const Login = () => {
+
+const Login = ({ login }) => {
     const [formData, setFormData] = useState({
         email: '',
         password:''
@@ -17,8 +21,7 @@ const Login = () => {
 
     const onSubmit = async e => {
         e.preventDefault();
-      
-            console.log('Sucess');
+      login(email,password);
         };
                 
         
@@ -64,4 +67,8 @@ const Login = () => {
     );
 };
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+}
+
+export default connect(null,{ login }) (Login);
