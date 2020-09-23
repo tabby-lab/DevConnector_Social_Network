475a0921c03fd677912e345c28f2b67f885c7ac3
Tabby Garcia <tabby.garcia1@gmail.com>
2020-09-23 13:40:38 -0500

    blessed up to have things working again.. implemented logout to reducer,auth action,navbar

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,7 +5,8 @@ import {
     USER_LOADED, 
     AUTH_ERROR,
     LOGIN_SUCCESS,
-    LOGIN_FAIL
+    LOGIN_FAIL,
+    LOGOUT
 } from './types';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
@@ -99,6 +100,16 @@ export const login= ( email, password ) => async dispatch => {
         });
     }
     };
+
+    //logout /clear profile
+
+    export const logout = () => dispatch => {
+        dispatch({ type: LOGOUT });
+    }
+
+
+
+
     
 
 
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
-const Navbar = () => {
-    return (
-        <nav className="navbar bg-dark">
-        <h1>
-          <Link to='/'>
-            <i className="fas fa-code"></i> DevConnector
-            </Link>
-        </h1>
-        <ul>
+
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+const authLinks = (
+<ul>
         <li>
-          <a href='!#'>Developers</a>
+          <a onClick={logout} href='#!'>
+            <i className="fas fa-sign-out-alt"></i>{' '}
+           <span className="hide-sm"> Logout </span>
+           </a>
+          </li>
+        </ul>
+);
+
+const guestLinks = (
+<ul>
+        <li>
+          <a href='#!'>Developers</a>
           </li>
           <li>
           <Link to='/register'>Register</Link>
@@ -20,10 +29,30 @@ const Navbar = () => {
          <Link to='login'>Login</Link>
          </li>
         </ul>
+);
+
+    return (
+        <nav className="navbar bg-dark">
+        <h1>
+          <Link to='/'>
+            <i className="fas fa-code"></i> DevConnector
+            </Link>
+        </h1>
+    { !loading && (<Fragment>{ isAuthenticated ? authLinks: guestLinks }</Fragment>) }
+        
       </nav>
     )
 };
 
-export default Navbar
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, {logout})(Navbar);
 
 //dont use a href tags use Link from router dom
