45b7b5eab784b05bae7225d0f7b620ae921c7f88
Tabby Garcia <tabby.garcia1@gmail.com>
2020-09-24 19:31:14 -0500

    brought in private route to app.js

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
 
-const PrivateRoute = ({ component: Component, auth:{isAuthenticated, loading}, ...rest }) => (
-    <Route { ...rest } render={props => !isAuthenticated && !loading ? (<Redirect to='/login' />): (<Component {...props}/>)} />
+const PrivateRoute = ({ component: Component, auth: { isAuthenticated, loading }, ...rest }) => (
+    <Route {...rest} render={props => !isAuthenticated && !loading ? (<Redirect to='/login' />) : (<Component {...props} />)} />
 )
 
 
 
 
 PrivateRoute.propTypes = {
-auth: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired,
 }
 
 const mapStateToProps = state => ({
