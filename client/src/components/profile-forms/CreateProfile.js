64680d9c39a032401e077e38e94daab25a3e41c6
Tabby Garcia <tabby.garcia1@gmail.com>
2020-10-16 17:37:57 -0500

    made a create profile in components and put some feilds for the form a user would have to fill out. not done.

---
+++ client/src/components/profile-forms/CreateProfile.js
@@ -0,0 +1,33 @@
+import React, { useSate } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+
+const CreateProfile = props => {
+    const [formData,setFormData ] = useState({
+        company: '',
+        website: '',
+        location: '',
+        status: '',
+        skills: '',
+        githubusername:'',
+        bio: '',
+        twitter: '',
+        facebook: '',
+        linkedin: '',
+        youtube: '',
+        instagram: ''
+
+    });
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+CreateProfile.propTypes = {
+
+}
+
+export default CreateProfile
