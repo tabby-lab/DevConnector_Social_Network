5d8048ef930305fdae99fddd2f21b43270894f85
Tabby Garcia <tabby.garcia1@gmail.com>
2020-10-16 17:19:05 -0500

    created a clear profile type once a user is logged out it clears that profile and reloads the new users profile.addded btn to create profile if that user doesnt have a profile

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
     AUTH_ERROR,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
-    LOGOUT
+    LOGOUT,
+    CLEAR_PROFILE
 } from './types';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
@@ -104,7 +105,9 @@ export const login= ( email, password ) => async dispatch => {
     //logout /clear profile
 
     export const logout = () => dispatch => {
+        dispatch({ type: CLEAR_PROFILE });
         dispatch({ type: LOGOUT });
+        
     }
 
 
