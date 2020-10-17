2b990560ad748700b29c221a1559c7dfb7e689a3
Tabby Garcia <tabby.garcia1@gmail.com>
2020-10-17 13:32:18 -0500

    alert works and create profile works too yayayayayayay

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,9 +1,11 @@
 import React, { useState, Fragment } from 'react';
+import { Link,withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { createProfile } from '../../actions/profile';
 
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
     const [formData,setFormData ] = useState({
         company: '',
         website: '',
@@ -39,6 +41,11 @@ const CreateProfile = props => {
 
     const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
 
+    const onSubmit = e => {
+        e.preventDefault();
+        createProfile(formData, history);
+    }
+
 
     return (
         <Fragment>
@@ -50,7 +57,7 @@ const CreateProfile = props => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={e => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -164,7 +171,9 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
+    createProfile:  PropTypes.func.isRequired,
+};
 
-}
 
-export default CreateProfile
+
+export default connect (null, { createProfile })(withRouter(CreateProfile));
