8f5fa5841a0f6e344ec01cb92d34a8dcc50e3cd4
Tabby Garcia <tabby.garcia1@gmail.com>
2020-10-17 13:09:18 -0500

    adding functionality to create and update profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -26,4 +26,27 @@ export const getCurrentProfile = () => async dispatch => {
             }
         });
     }
-}
+};
+
+//CREATE OR UPDATE A PROFILE
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const res = await axios.post('/api/profile',formData, config);
+        dispatch({
+            type:GET_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created'));
+
+        
+    } catch (err) {
+        
+    }
+} 
