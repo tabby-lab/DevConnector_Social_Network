dc9b1c33b6368160c68fc6c573ad5bbe00b23462
Tabby Garcia <tabby.garcia1@gmail.com>
2020-10-23 15:25:33 -0500

    destructored all feilds and made onchanges to them. also added toggle for current

--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -15,7 +15,13 @@ const AddExperience = props => {
         current: false,
         description: ''
      });
-     
+
+     const [toDateDisabled, toggleDisabled] = useState(false);
+
+     const { company, title, location, from, to, current, description } = formData;
+
+     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
     return (
         <Fragment>
              <h1 class="large text-primary">
@@ -28,24 +34,33 @@ const AddExperience = props => {
       <small>* = required field</small>
       <form class="form">
         <div class="form-group">
-          <input type="text" placeholder="* Job Title" name="title" required />
+          <input type="text" placeholder="* Job Title" name="title" value={title}
+          onChange ={e => onChange(e)}  required />
         </div>
         <div class="form-group">
-          <input type="text" placeholder="* Company" name="company" required />
+          <input type="text" placeholder="* Company" name="company" value={company}
+          onChange ={e => onChange(e)}required />
         </div>
         <div class="form-group">
-          <input type="text" placeholder="Location" name="location" />
+          <input type="text" placeholder="Location" name="location" value={location}
+          onChange ={e => onChange(e)}/>
         </div>
         <div class="form-group">
           <h4>From Date</h4>
-          <input type="date" name="from" />
+          <input type="date" name="from" value={from}
+          onChange ={e => onChange(e)}/>
         </div>
          <div class="form-group">
-          <p><input type="checkbox" name="current" value="" /> Current Job</p>
+          <p><input type="checkbox" name="current" checked={current} value="" value={current} onChange={e => {
+              setFormData({ ...formData, current: !current });
+              toggleDisabled(!toDateDisabled);
+          }}
+         /> Current Job</p>
         </div>
         <div class="form-group">
           <h4>To Date</h4>
-          <input type="date" name="to" />
+          <input type="date" name="to" value={to}
+          onChange ={e => onChange(e)}/>
         </div>
         <div class="form-group">
           <textarea
@@ -53,6 +68,8 @@ const AddExperience = props => {
             cols="30"
             rows="5"
             placeholder="Job Description"
+            value={description}
+          onChange ={e => onChange(e)}
           ></textarea>
         </div>
         <input type="submit" class="btn btn-primary my-1" />
