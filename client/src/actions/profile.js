4190035633bd8e7ef6c2d6e1e2b049a07e207e26
Tabby Garcia <tabby.garcia1@gmail.com>
2020-10-29 10:05:46 -0500

    delete account  in profile actions

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -4,7 +4,9 @@ import { setAlert } from './alert';
 import {
     GET_PROFILE,
     PROFILE_ERROR,
-    UPDATE_PROFILE
+    UPDATE_PROFILE,
+    ACCOUNT_DELETED,
+    CLEAR_PROFILE
 } from './types';
 
 //Get current users profile
@@ -189,4 +191,38 @@ export const deleteEducation = id => async dispatch => {
         });
         
     }
-}
+};
+
+//Delete Account & Profile
+export const deleteAccount = () => async dispatch => {
+    if (window.confirm('Are you sure? This can NOT be undone.')) {
+
+
+        try {
+            const res = await axios.delete('/api/profile');
+    
+            dispatch({
+                type: CLEAR_PROFILE
+            });
+            dispatch({
+                type: ACCOUNT_DELETED
+            });
+    
+            dispatch(setAlert('Your account has been deleted.'));
+    
+        } catch (err) {
+            dispatch({
+                
+                type:PROFILE_ERROR,
+                payload: {
+                    msg:err.response.statusText,
+                    status: err.response.status
+                }
+            });
+            
+        }
+    }
+
+
+   
+};
