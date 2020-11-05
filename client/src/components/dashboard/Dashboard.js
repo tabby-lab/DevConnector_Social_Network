60302e7acccb123d836c7e7cc9b5fd51be47e37b
Tabby Garcia <tabby.garcia1@gmail.com>
2020-11-05 13:17:56 -0600

    delete account works. Brought it into my dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,14 +2,14 @@ import React, { Fragment, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getCurrentProfile } from '../../actions/profile';
+import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 import Spinner from '../layout/Spinner';
 import PrivateRoute from '../routing/PrivateRoute';
 import DashboardActions from './DashboardActions';
 import Experience from './Experience';
 import Education from './Education';
 
-const Dashboard = ({ getCurrentProfile, auth: { user },  profile:{ profile,loading }}) => {
+const Dashboard = ({ getCurrentProfile, deleteAccount, auth: { user },  profile:{ profile,loading }}) => {
     useEffect(()=>{
         getCurrentProfile();
     }, []);
@@ -22,6 +22,11 @@ const Dashboard = ({ getCurrentProfile, auth: { user },  profile:{ profile,loadi
     <DashboardActions/>
     <Experience experience={profile.experience} />
     <Education education={profile.education} />
+    
+    <div className="my-2"></div>
+    <button className="btn btn-danger" onClick={()=> deleteAccount()}>
+        <i className="fas fa-user-minus"></i>Delete My Account
+    </button>
 </Fragment> 
 ): (
 <Fragment>
@@ -37,6 +42,7 @@ Dashboard.propTypes = {
 getCurrentProfile: PropTypes.func.isRequired,
 auth: PropTypes.object.isRequired,
 profile: PropTypes.object.isRequired,
+deleteAccount: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
@@ -44,5 +50,5 @@ const mapStateToProps = state => ({
     profile:state.profile
 })
 
-export default connect(mapStateToProps,{ getCurrentProfile })(Dashboard)
+export default connect(mapStateToProps,{ getCurrentProfile, deleteAccount })(Dashboard)
 
