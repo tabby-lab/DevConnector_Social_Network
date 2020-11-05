712e4fa793ab99f20046b9c0807c5abb0b106d61
Tabby Garcia <tabby.garcia1@gmail.com>
2020-11-05 12:26:17 -0600

    delete education works now. was missing a / in actions profile js

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -171,7 +171,7 @@ export const deleteExperience = id => async dispatch => {
 //Delete Education
 export const deleteEducation = id => async dispatch => {
     try {
-        const res = await axios.delete(`/api/profile/education${id}`);
+        const res = await axios.delete(`/api/profile/education/${id}`);
 
         dispatch({
             type: UPDATE_PROFILE,
