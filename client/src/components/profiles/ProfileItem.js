968504b7b263b5db652e5e8cb88ebba9717b8847
Tabby Garcia <tabby.garcia1@gmail.com>
2020-11-08 12:05:58 -0600

    working on profile item layout. I have the name, status, company, location, and a link to the id. now i need to add the skills after the link

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const ProfileItem = props => {
-    return (
-        <div>
-          test  
-        </div>
-    )
+const ProfileItem = ({
+    profile: {
+    user: { _id, name, avatar },
+    status,
+    company,
+    location,
+    skills
 }
+ }) => {
+    return <div className="profile bg-light">
+       <img src={avatar} alt="" className="round-img"/> 
+       <div>
+ <h2>{name}</h2>
+ <p>{status} {company && <span> at {company}</span>}</p>
+ <p className="my-1">{location && <span>{location}</span>}</p>
+ <Link to={`/profile/${_id}`} className='btn btn-primary'>
+     View Profile
+ </Link>
+       </div>
+        </div>
+};
 
 ProfileItem.propTypes = {
-
+profile: PropTypes.object.isRequired,
 }
 
 export default ProfileItem
