1b4dbac13496dd696739bbc14f5c25d46a8dea4a
Tabby Garcia <tabby.garcia1@gmail.com>
2020-11-13 11:58:29 -0600

    finally fixed bug in posts. now inserting dynamically name, avatar, likes, comments, and date to postitem

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -6,36 +6,37 @@ import Spinner from '../layout/Spinner';
 import PostItem from './PostItem';
 import { post } from 'request';
 
-const Posts = ({ getPosts, post: { posts, loading } }) => {
+const Posts = ({ getPosts, post: { posts } }) => {
     useEffect(()=> {
         getPosts();
     }, [getPosts]);
 
-    return loading ? <Spinner /> : (
+    return (
         <Fragment>
             <h1 className='large text-primary'>Posts</h1>
             <p className="lead">
-                <i className="fas fa-user"></i>Welcome to the community.
+                <i className="fas fa-user"></i>Welcome to the community
             </p>
           
-
+        
             <div className="posts">
-                {post.map(post => (
+                {posts.map((post) => (
                     <PostItem key={post._id} post={post} />
+                    
                 ))}
-            </div>
+           </div>
         </Fragment>
-    ) ;
+    );
    
 };
 
 Posts.propTypes = {
     getPosts: PropTypes.func.isRequired,
     post: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = (state) => ({ 
     post: state.post
  });
 
-export default connect(mapStateToProps, {getPosts})(Posts)
+export default connect(mapStateToProps, {getPosts})(Posts);
