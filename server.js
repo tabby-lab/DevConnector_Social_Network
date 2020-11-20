deb3d48b8aeff082cb13992c6af704b82fd19bff
Tabby Garcia <tabby.garcia1@gmail.com>
2020-11-20 09:15:34 -0600

    ready to deploy to heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -12,13 +13,20 @@ app.use(express.json({extended: false}));
 
 
 
-app.get('/', (req, res)=> res.send('API running'));
-
 //access the routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+//SERVE STATIC ASSERS IN PRODUCTION
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
